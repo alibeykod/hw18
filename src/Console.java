847b15ad321d1d85b2fc335bdee92f49dc37f66f
beykod <alirafibeyk@gmail.com>
2026-07-19 14:07:28 +0330

    feat: Create Database and tables that we need

---
+++ src/Console.java
@@ -0,0 +1,19 @@
+public class Console {
+/*
+    CREATE DATABASE warehouse_product_management_system;
+
+    CREATE TABLE products(
+            id BIGSERIAL PRIMARY KEY  ,
+            name VARCHAR ,
+            price DECIMAL ,
+            quantity INTEGER
+    );
+
+    CREATE TABLE supplier(
+            id BIGSERIAL PRIMARY KEY ,
+            company_name VARCHAR ,
+            phone VARCHAR
+    );
+*/
+
+}
