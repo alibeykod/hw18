4110eade76ee8a6b17b225d37dff717f51002a44
beykod <alirafibeyk@gmail.com>
2026-07-19 14:31:43 +0330

    feat: add database config fields url and username and password

--- src/util/DatabaseConfig.java
+++ src/util/DatabaseConfig.java
@@ -1,5 +1,8 @@
 package util;
 
 public class DatabaseConfig {
+    private static final String url = "jdbc:postgresql://localhost:5432/warehouse_product_management_system";
+    private static final String username = "postgres";
+    private static final String password = "20001397";
 
 }
