54625c3ff33f9d6678b62ba2b119f35a3e9ae37b
beykod <alirafibeyk@gmail.com>
2026-07-19 14:09:28 +0330

    feat: Create a product model with field and getters and setters and hashcode and equals

---
+++ src/model/Product.java
@@ -0,0 +1,69 @@
+package model;
+
+import java.util.Objects;
+
+public class Product {
+    private Long id;
+    private String name;
+    private Double price;
+    private int quantity;
+
+    public Product(Long id, String name, Double price, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    // a constructor without id :
+
+    public Product(String name, Double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return quantity == product.quantity && Objects.equals(id, product.id) && Objects.equals(name, product.name) && Objects.equals(price, product.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, price, quantity);
+    }
+}
