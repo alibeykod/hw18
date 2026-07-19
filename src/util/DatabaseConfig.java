b60bfe09a17e9c2177f2750435fd06ae883cd3de
beykod <alirafibeyk@gmail.com>
2026-07-19 14:36:16 +0330

    feat: add a method to connect to Database

--- src/util/DatabaseConfig.java
+++ src/util/DatabaseConfig.java
@@ -1,8 +1,22 @@
 package util;
 
+import exceptions.DatabaseConfigConnectionException;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConfig {
     private static final String url = "jdbc:postgresql://localhost:5432/warehouse_product_management_system";
     private static final String username = "postgres";
     private static final String password = "20001397";
 
+
+    public static Connection getconnection(){
+        try {
+            return DriverManager.getConnection(url , username , password);
+        } catch (SQLException e) {
+            throw new DatabaseConfigConnectionException("Connection Failed !");
+        }
+    }
 }
